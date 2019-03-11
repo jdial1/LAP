df322ee0d9c60cc3754e0e8c8e4ceef5bd9cc783
Justin Dial <justinalandial@gmail.com>
2019-03-11 12:17:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.static("."));
 
 //Index Route
 app.get('/', (request, response) => {
-  response.sendFile(path.join(__dirname, "./index.html"));
+	response.sendFile(path.join(__dirname, "./index.html"));
 });
 
 // Initialize app
@@ -16,26 +16,30 @@ const server = app.listen(port, () => console.log('App Started'));
 
 const io = require('socket.io')(server);
 
-var clients =[];
+var clients = [];
 
-io.on('connection', function(socket) {
+io.on('connection', function (socket) {
 
-    socket.on('GET_CLIENT_ID',function(){
-      console.log('GET_CLIENT_ID');
-      console.log('NEW CLIENT: '+socket.id);
-      io.to(socket.id).emit('CLIENT_ID',socket.id);
-    });
+	socket.on('GET_CLIENT_ID', function () {
+		console.log('GET_CLIENT_ID');
+		console.log('NEW CLIENT: ' + socket.id);
+		io.to(socket.id).emit('CLIENT_ID', socket.id);
+	});
 
-    socket.on('SEND_GUESS', function(data) {
-        console.log('SEND_GUESS');
-        console.log(data);
-        console.log('SENDING to:'+data[5]);
-        if(data[5] != 0){io.to(data[5]).emit('GUESS',data)};
-    });
+	socket.on('SEND_GUESS', function (data) {
+		console.log('SEND_GUESS');
+		console.log(data);
+		console.log('SENDING to:' + data[5]);
+		if (data[5] != 0) {
+			io.to(data[5]).emit('GUESS', data)
+		};
+	});
 
-    socket.on('CLIENT_GUESS_RESPONSE', function(data) {
-      console.log('CLIENT_GUESS_RESPONSE',data);
-      console.log('Sending SERVER_GUESS_RESPONSE to: ',data[3]);
-      if(data[3] != 0){io.to(data[3]).emit('SERVER_GUESS_RESPONSE',data)};
-    });
+	socket.on('CLIENT_GUESS_RESPONSE', function (data) {
+		console.log('CLIENT_GUESS_RESPONSE', data);
+		console.log('Sending SERVER_GUESS_RESPONSE to: ', data[3]);
+		if (data[3] != 0) {
+			io.to(data[3]).emit('SERVER_GUESS_RESPONSE', data)
+		};
+	});
 });

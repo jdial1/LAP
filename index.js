339212de7eab4e814134ae6aa02d051cdc4b989a
Justin Dial <justinalandial@gmail.com>
2019-03-12 20:02:30 -0500

    1

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.static("."));
 
 //Index Route
 app.get('/', (request, response) => {
-	response.sendFile(path.join(__dirname, "./index.html"));
+  response.sendFile(path.join(__dirname, "./index.html"));
 });
 
 // Initialize app
@@ -16,30 +16,47 @@ const server = app.listen(port, () => console.log('App Started'));
 
 const io = require('socket.io')(server);
 
-var clients = [];
+var animals = [
+  'Bear','Cat','Duck','Elephant','Giraffe','Hamster',
+  'Iguana','Jerboa','Kangaroo','Manatee','Numbat',
+  'Otter','Penguin','Quokka','Rabbit','Sheep',
+  'Tortoise','Monkey','Weasel','Zebra'
+];
+var clients ={};
+var online =0;
+io.on('connection', function(socket) {
 
-io.on('connection', function (socket) {
+    console.log(animals[2]);
+    random_animal=animals[Math.floor(Math.random()*animals.length)]+Math.floor(Math.random()*Math.floor(987));
+    for (var i = 0; i < clients.length; i++) {
+        if (random_animal=clients[i]){
+          random_animal=animals[Math.floor(Math.random()*animals.length)]+Math.floor(Math.random()*Math.floor(978));
+        }
+    }
+    clients[socket.id] =random_animal;
+    console.log('GET_CLIENT_ID Animal: '+clients[socket.id]);
+    console.log('NEW CLIENT: '+clients[socket.id]);
+    online++;
+    console.log('Client List: '+JSON.stringify(clients,null,1));
+    console.log('Online: '+online);
+    io.to(socket.id).emit('CLIENT_ID',clients[socket.id]);
 
-	socket.on('GET_CLIENT_ID', function () {
-		console.log('GET_CLIENT_ID');
-		console.log('NEW CLIENT: ' + socket.id);
-		io.to(socket.id).emit('CLIENT_ID', socket.id);
-	});
+    socket.on('SEND_GUESS', function(data) {
+        console.log('SEND_GUESS');
+        console.log(data);
+        console.log('SENDING to:'+data[5]);
+        if(data[5] != 0){io.to(data[5]).emit('GUESS',data)};
+    });
 
-	socket.on('SEND_GUESS', function (data) {
-		console.log('SEND_GUESS');
-		console.log(data);
-		console.log('SENDING to:' + data[5]);
-		if (data[5] != 0) {
-			io.to(data[5]).emit('GUESS', data)
-		};
-	});
+    socket.on('CLIENT_GUESS_RESPONSE', function(data) {
+      console.log('CLIENT_GUESS_RESPONSE',data);
+      console.log('Sending SERVER_GUESS_RESPONSE to: ',data[3]);
+      if(data[3] != 0){io.to(data[3]).emit('SERVER_GUESS_RESPONSE',data)};
+    });
 
-	socket.on('CLIENT_GUESS_RESPONSE', function (data) {
-		console.log('CLIENT_GUESS_RESPONSE', data);
-		console.log('Sending SERVER_GUESS_RESPONSE to: ', data[3]);
-		if (data[3] != 0) {
-			io.to(data[3]).emit('SERVER_GUESS_RESPONSE', data)
-		};
-	});
+    socket.on('disconnect', function () {
+      socket.emit('disconnected');
+      online = online - 1;
+      delete clients[socket.id];
+    });
 });
--- public/index.js
+++ public/index.js
@@ -1,199 +1,185 @@
 /* Use Matrice math to look for connected sections */
 window.onload = function () {
-	Vue.use(new VueSocketIO({
-		connection: 'http://192.168.1.248:8080'
-	}));
-	new Vue({
-		el: "#app",
-		data: {
-			tcolumns: [' ', 1, 2, 3, 4, 5, 6, 7, 8],
-			socket_id_num: "NO USER ID",
-			op_id: "Opponents_ID",
-			letter_to_num: {
-				'A': 0,
-				'B': 1,
-				'C': 2,
-				'D': 3,
-				'E': 4,
-				'F': 5,
-				'G': 6,
-				'H': 7
-			},
-			header_rows: ['A ', 'B ', 'C ', 'D ', 'E ', 'F ', 'G ', 'H '],
-			f_col: '1',
-			s_col: '2',
-			f_row: 'A',
-			s_row: 'B',
-			my_grid_seen: 1,
-			op_grid_seen: 1,
-			my_gridnum_seen: 1,
-			op_gridnum_seen: 1,
-			my_grid: [
-				[1, 0, 3, 0, 0, 4, 0, 4],
-				[2, 0, 0, 4, 4, 4, 0, 4],
-				[3, 3, 0, 1, 0, 4, 0, 4],
-				[4, 3, 3, 1, 1, 4, 4, 4],
-				[1, 3, 1, 1, 0, 0, 3, 0],
-				[2, 3, 2, 0, 0, 1, 0, 0],
-				[3, 3, 2, 0, 2, 0, 4, 0],
-				[4, 0, 2, 0, 4, 0, 0, 4]
-			],
-			op_grid: [
-				[1, 0, 3, 0, 0, 4, 0, 0],
-				[1, 0, 0, 4, 4, 2, 3, 0],
-				[1, 0, 0, 1, 0, 2, 0, 3],
-				[1, 0, 2, 0, 1, 4, 3, 0],
-				[1, 0, 0, 1, 0, 0, 3, 0],
-				[2, 0, 2, 0, 0, 2, 0, 0],
-				[3, 1, 0, 0, 2, 0, 4, 0],
-				[4, 0, 2, 0, 1, 0, 0, 4]
-			]
-		},
-		methods: {
-			button_get_id: function () {
-				console.log('Getting ID');
-				this.$socket.emit('GET_CLIENT_ID');
-			},
+  Vue.use(new VueSocketIO({connection: 'http://192.168.1.236:8080'}));
+  new Vue({
+    el: "#app",
+    data: {
+      tcolumns:[' ',1,2,3,4,5,6,7,8],
+      socket_id_num:"NO USER ID",
+      op_id:"Opponents_ID",
+      letter_to_num : {'A':0,'B':1,'C':2,'D':3,'E':4,'F':5,'G':6,'H':7},
+      header_rows:['A ','B ','C ','D ','E ','F ','G ','H '],
+      f_col:'1',
+      s_col:'2',
+      f_row:'A',
+      s_row:'B',
+      my_grid_seen:1,
+      op_grid_seen:1,
+      my_gridnum_seen:1,
+      op_gridnum_seen:1,
+      my_grid:
+      [
+        [1,0,3,0,0,4,0,4],
+        [2,0,0,4,4,4,0,4],
+        [3,3,0,1,0,4,0,4],
+        [4,3,3,1,1,4,4,4],
+        [1,3,1,1,0,0,3,0],
+        [2,3,2,0,0,1,0,0],
+        [3,3,2,0,2,0,4,0],
+        [4,0,2,0,4,0,0,4]
+      ],
+      op_grid:
+      [
+        [1,0,3,0,0,4,0,0],
+        [1,0,0,4,4,2,3,0],
+        [1,0,0,1,0,2,0,3],
+        [1,0,2,0,1,4,3,0],
+        [1,0,0,1,0,0,3,0],
+        [2,0,2,0,0,2,0,0],
+        [3,1,0,0,2,0,4,0],
+        [4,0,2,0,1,0,0,4]
+      ]
+    },
+    methods: {
+      button_get_id: function(){
+        console.log('Getting ID');
+        this.$socket.emit('GET_CLIENT_ID');
+      },
 
-			button_guess_sent: function () {
-				this.$socket.emit('SEND_GUESS', [this.f_col - 1, this.s_col - 1, this.f_row, this.s_row, this.socket_id_num, this.op_id]);
-			},
-			clear_my_grid: function () {
-				this.my_grid = [
-					[0, 0, 0, 0, 0, 0, 0, 0],
-					[0, 0, 0, 0, 0, 0, 0, 0],
-					[0, 0, 0, 0, 0, 0, 0, 0],
-					[0, 0, 0, 0, 0, 0, 0, 0],
-					[0, 0, 0, 0, 0, 0, 0, 0],
-					[0, 0, 0, 0, 0, 0, 0, 0],
-					[0, 0, 0, 0, 0, 0, 0, 0],
-					[0, 0, 0, 0, 0, 0, 0, 0],
-				];
-			},
-			clear_op_grid: function () {
-				this.op_grid = [
-					[0, 0, 0, 0, 0, 0, 0, 0],
-					[0, 0, 0, 0, 0, 0, 0, 0],
-					[0, 0, 0, 0, 0, 0, 0, 0],
-					[0, 0, 0, 0, 0, 0, 0, 0],
-					[0, 0, 0, 0, 0, 0, 0, 0],
-					[0, 0, 0, 0, 0, 0, 0, 0],
-					[0, 0, 0, 0, 0, 0, 0, 0],
-					[0, 0, 0, 0, 0, 0, 0, 0],
-				];
-			},
-			chk_nbrs: function (grid, row, col, side, number) {
+      button_guess_sent: function(){
+        this.$socket.emit('SEND_GUESS', [this.f_col-1,this.s_col-1,this.f_row,this.s_row,this.socket_id_num,this.op_id]);
+      },
+    	clear_my_grid: function () {
+  			this.my_grid = [
+          [0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0],
+      	];
+    	},
+     clear_op_grid: function () {
+  			this.op_grid = [
+          [0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0],
+      	];
+    	},
+      chk_nbrs: function(grid,row,col,side,number){
 
-				// console.log('------');
-				// console.log('row: '+row);
-				// console.log('col: '+col);
-				// console.log('side: '+side);
-				// console.log('number: '+number);
-				// console.log('------');
+        // console.log('------');
+        // console.log('row: '+row);
+        // console.log('col: '+col);
+        // console.log('side: '+side);
+        // console.log('number: '+number);
+        // console.log('------');
 
-				if (side == 'left') {
-					if (col == 0) {
-						return true
-					};
-					if (grid[row][col - 1] == number) {
-						return false
-					};
-					return true;
-				}
-				if (side == 'right') {
-					if (col == 7) {
-						return true
-					};
-					if (grid[row][col + 1] == number) {
-						return false
-					};
-					return true;
-				}
-				if (side == 'top') {
-					if (row == 0) {
-						return true
-					};
-					if (grid[row - 1][col] == number) {
-						return false
-					};
-					return true;
-				}
-				if (side == 'bottom') {
-					if (row == 7) {
-						return true
-					};
-					if (grid[row + 1][col] == number) {
-						return false
-					};
-					return true;
-				}
-			},
-			set_grid: function (col_one, col_two, row_one, row_two, grid_nums) {
-				console.log('SETTING GRID: ', col_one, col_two, row_one, row_two, grid_nums)
-				this.op_grid[row_one][col_one + 1] = grid_nums[0];
-				this.op_grid[row_one][col_two + 1] = grid_nums[1];
-				this.op_grid[row_two][col_one + 1] = grid_nums[2];
-				this.op_grid[row_two][col_two + 1] = grid_nums[3];
-				this.$forceUpdate();
-			},
-			get_grid: function (col_one, col_two, row_one, row_two) {
+        if (side == 'left'){
+          if (col == 0){return true};
+          if (grid[row][col-1] == number){return false};
+          return true;
+        }
+        if (side == 'right'){
+          if (col == 7){return true};
+          if (grid[row][col+1] == number){return false};
+          return true;
+        }
+        if (side == 'top'){
+          if (row == 0){return true};
+          if (grid[row-1][col] == number){return false};
+          return true;
+        }
+        if (side == 'bottom'){
+          if (row == 7){return true};
+          if (grid[row+1][col] == number){return false};
+          return true;
+        }
+      },
+      set_grid:function(col_one,col_two,row_one,row_two,grid_nums){
+        console.log('SETTING GRID: ',col_one,col_two,row_one,row_two,grid_nums)
+        this.op_grid[row_one][col_one+1]=grid_nums[0];
+        this.op_grid[row_one][col_two+1]=grid_nums[1];
+        this.op_grid[row_two][col_one+1]=grid_nums[2];
+        this.op_grid[row_two][col_two+1]=grid_nums[3];
+        this.$forceUpdate();
+      },
+      get_grid:function(col_one,col_two,row_one,row_two){
 
-				console.log('Get_grid1: ' + col_one);
-				console.log('Get_grid2: ' + col_two);
-				console.log('Get_grid3: ' + row_one);
-				console.log('Get_grid4: ' + row_two);
-				console.log('to_letter_value1: ' + this.letter_to_num[row_one]);
-				console.log('to_letter_value2: ' + this.letter_to_num[row_two]);
-				console.log(this.my_grid[col_one][this.letter_to_num[row_one]]);
-				let grid_nums = [];
-				grid_nums[0] = this.my_grid[this.letter_to_num[row_one]][col_one];
-				grid_nums[1] = this.my_grid[this.letter_to_num[row_one]][col_two];
-				grid_nums[2] = this.my_grid[this.letter_to_num[row_two]][col_one];
-				grid_nums[3] = this.my_grid[this.letter_to_num[row_two]][col_two];
-				console.log(grid_nums);
-				return grid_nums;
-			}
-		},
+        console.log('Get_grid1: '+col_one);
+        console.log('Get_grid2: '+col_two);
+        console.log('Get_grid3: '+row_one);
+        console.log('Get_grid4: '+row_two);
+        console.log('to_letter_value1: '+this.letter_to_num[row_one]);
+        console.log('to_letter_value2: '+this.letter_to_num[row_two]);
+        console.log(this.my_grid[col_one][this.letter_to_num[row_one]]);
+        let grid_nums=[];
+        grid_nums[0]=this.my_grid[this.letter_to_num[row_one]][col_one];
+        grid_nums[1]=this.my_grid[this.letter_to_num[row_one]][col_two];
+        grid_nums[2]=this.my_grid[this.letter_to_num[row_two]][col_one];
+        grid_nums[3]=this.my_grid[this.letter_to_num[row_two]][col_two];
+        console.log(grid_nums);
+        return grid_nums;
+      }
+   },
 
-		sockets: {
-			CLIENT_ID: function (data) {
-				console.log('connection: ' + data);
-				this.socket_id_num = data;
-			},
-			SERVER_GUESS_RESPONSE: function (data) {
-				console.log(data);
-				console.log('data: ' + data);
-				let grid_pos = data[0];
-				let grid_nums = data[1];
-				console.log('grid_pos: ' + grid_pos);
-				console.log('grid_nums: ' + grid_nums);
+  sockets:{
+    //Client sends GET_CLIENT_ID to server
 
-				console.log('letter_to_num: ' + grid_pos[2]);
-				console.log('letter_to_num: ' + this.letter_to_num[grid_pos[2]]);
-				console.log('grid_pos: ' + grid_pos[2]);
+    //Player 1 sends SEND_GUESS to server
+    //Server sends GUESS to Player 2
+    //Player2 sends CLIENT_GUESS_RESPONSE to Server
+    //Server sends SERVER_GUESS_RESPONSE to Player 1
 
-				let col_one = grid_pos[0] - 1;
-				let col_two = grid_pos[1] - 1;
-				let row_one = this.letter_to_num[grid_pos[2]];
-				let row_two = this.letter_to_num[grid_pos[3]];
-				console.log(col_one);
-				console.log(col_two);
-				console.log(row_one);
-				console.log(row_two);
+    CLIENT_ID:function(data){
+        console.log('connection: '+data);
+        this.socket_id_num=data;
+    },
 
-				console.log(this.op_grid);
-				console.log("COMPLETED GUESS RESPONSE:  " + data);
-				this.set_grid(col_one, col_two, row_one, row_two, grid_nums);
-			},
-			GUESS: function (data) {
-				console.log('REQUESTED GUESS: ' + data);
-				this.op_id = data[4];
-				let coords = [data[0], data[1], data[2], data[3]];
-				console.log('GET GRID:', data[0], data[1], data[2], data[3]);
-				data = [coords, this.get_grid(data[0], data[1], data[2], data[3]), this.socket_id_num, this.op_id];
-				console.log('SENDING: ' + data);
-				this.$socket.emit('CLIENT_GUESS_RESPONSE', data);
-			},
-		},
-	})
+    
+    SERVER_GUESS_RESPONSE:function(data){
+      console.log(data);
+      console.log('data: '+data);
+      let grid_pos=data[0];
+      let grid_nums=data[1];
+      console.log('grid_pos: '+grid_pos);
+      console.log('grid_nums: '+grid_nums);
+
+      console.log('letter_to_num: '+grid_pos[2]);
+      console.log('letter_to_num: '+this.letter_to_num[grid_pos[2]]);
+      console.log('grid_pos: '+grid_pos[2]);
+
+      let col_one = grid_pos[0]-1;
+      let col_two = grid_pos[1]-1;
+      let row_one = this.letter_to_num[grid_pos[2]];
+      let row_two = this.letter_to_num[grid_pos[3]];
+      console.log(col_one);
+      console.log(col_two);
+      console.log(row_one);
+      console.log(row_two);
+
+      console.log(this.op_grid);
+      console.log("COMPLETED GUESS RESPONSE:  "+data);
+      this.set_grid(col_one,col_two,row_one,row_two,grid_nums);
+    },
+
+
+    GUESS:function(data){
+        console.log('REQUESTED GUESS: '+data);
+        this.op_id=data[4];
+        let coords=[data[0],data[1],data[2],data[3]];
+        console.log('GET GRID:',data[0],data[1],data[2],data[3]);
+        data=[coords,this.get_grid(data[0],data[1],data[2],data[3]),this.socket_id_num,this.op_id];
+        console.log('SENDING: '+data);
+        this.$socket.emit('CLIENT_GUESS_RESPONSE',data);
+    },
+  },
+})
 }

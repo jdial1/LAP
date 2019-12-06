2edd65c41d1d93ff8046914034e9a5bd2b75ddc3
Unhack AI <cos+a@unhack.ai>
2019-12-06 14:49:33 +0000

    Remove `console.log` Remove `console.log` statements.

--- public/index.js
+++ public/index.js
@@ -48,7 +48,6 @@ window.onload = () => {
     methods: {
       button_get_id: function(){
 
-        console.log('Getting ID');
         this.$socket.emit('GET_CLIENT_ID');
       },
 
@@ -56,14 +55,12 @@ window.onload = () => {
 
         if(this.active_player_toggle !== 0 ){
           this.$socket.emit('SEND_GUESS', [this.f_col-1,this.s_col-1,this.f_row,this.s_row,this.my_id,this.op_id]);
-          console.log('player guessed');
         }
         this.active_player_toggle=0;
       },
 
       looking_button_sent: function(){
         this.look_for_op=!this.look_for_op;
-        console.log({'name':this.my_id,'looking_for_opp':this.look_for_op});
         this.$socket.emit('SET_LOOKING_FOR_OPP_FLAG', {'name':this.my_id,'looking_for_opp':this.look_for_op});
       },
 
@@ -122,8 +119,6 @@ window.onload = () => {
 
       set_grid:function(col_one,col_two,row_one,row_two,grid_nums){
 
-        console.log('SETTING GRID: ',col_one,col_two,row_one,row_two,grid_nums);
-
         this.op_grid[row_one][col_one+1]=grid_nums[0];
         this.op_grid[row_one][col_two+1]=grid_nums[1];
         this.op_grid[row_two][col_one+1]=grid_nums[2];
@@ -133,22 +128,12 @@ window.onload = () => {
 
       get_grid:function(col_one,col_two,row_one,row_two){
 
-        console.log('Get_grid1: '+col_one);
-        console.log('Get_grid2: '+col_two);
-        console.log('Get_grid3: '+row_one);
-        console.log('Get_grid4: '+row_two);
-        console.log('to_letter_value1: '+this.letter_to_num[row_one]);
-        console.log('to_letter_value2: '+this.letter_to_num[row_two]);
-        console.log(this.my_grid[col_one][this.letter_to_num[row_one]]);
-
         let grid_nums=[];
 
         grid_nums[0]=this.my_grid[this.letter_to_num[row_one]][col_one];
         grid_nums[1]=this.my_grid[this.letter_to_num[row_one]][col_two];
         grid_nums[2]=this.my_grid[this.letter_to_num[row_two]][col_one];
         grid_nums[3]=this.my_grid[this.letter_to_num[row_two]][col_two];
-
-        console.log(grid_nums);
 
         return grid_nums;
       }
@@ -158,57 +143,32 @@ window.onload = () => {
 
       CLIENT_ID:function(data){
 
-        console.log('connection: '+data.name);
         this.my_id=data.name;
       },
 
       SERVER_GUESS_RESPONSE:function(data){
 
-        console.log(data);
-        console.log('data: '+data);
         let grid_pos=data[0];
         let grid_nums=data[1];
-        console.log('grid_pos: '+grid_pos);
-        console.log('grid_nums: '+grid_nums);
-
-        console.log('letter_to_num: '+grid_pos[2]);
-        console.log('letter_to_num: '+this.letter_to_num[grid_pos[2]]);
-        console.log('grid_pos: '+grid_pos[2]);
 
         let col_one = grid_pos[0]-1;
         let col_two = grid_pos[1]-1;
         let row_one = this.letter_to_num[grid_pos[2]];
         let row_two = this.letter_to_num[grid_pos[3]];
-
-        console.log(col_one);
-        console.log(col_two);
-        console.log(row_one);
-        console.log(row_two);
-
-        console.log(this.op_grid);
-        console.log("COMPLETED GUESS RESPONSE:  "+data);
         this.set_grid(col_one,col_two,row_one,row_two,grid_nums);
       },
 
       GUESS:function(data){
 
-        console.log('REQUESTED GUESS: '+data);
-        console.log('data4',data[4]);
         this.op_id=data[4];
-        console.log('op',this.op_id);
         let coords=[data[0],data[1],data[2],data[3]];
-        console.log('GET GRID:',data[0],data[1],data[2],data[3]);
         data=[coords,this.get_grid(data[0],data[1],data[2],data[3]),this.my_id,this.op_id];
-        console.log('SENDING: '+data);
         this.$socket.emit('CLIENT_GUESS_RESPONSE',data);
         this.active_player_toggle=1;
       },
       USER_COUNT_UPDATE:function(data){
-        console.log('User Count: ',data[1]);
         this.users=Object.keys(data[1]).length;
-        console.log(this.my_id);
         delete data[1][this.my_id];
-        console.log(data[1][this.my_id]);
         this.users_list=data[1];
       },
   },
